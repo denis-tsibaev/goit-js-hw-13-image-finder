dc69cd3d94393d37e4646137e56badefc77aae63
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-07-01 23:39:30 +0700

    Заполнение разметки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+
+// const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=22318307-8fc961fa8d00a621cd6d86864`;
+
+fetch(URL)
+  .then(response => response.json())
+  .then(console.log);
