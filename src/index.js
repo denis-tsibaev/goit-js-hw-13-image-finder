34c146d368ff4ed67d815e36631b83411ee9036f
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-07-02 09:02:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './sass/main.scss';
 
-// const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=22318307-8fc961fa8d00a621cd6d86864`;
+const options = {
+	headers: {
+		Authorization: '22318307-8fc961fa8d00a621cd6d86864',
+	},
+};
 
-fetch(URL)
+const url =
+`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=dog&page=1&per_page=12`;
+
+
+fetch(url, options)
   .then(response => response.json())
-  .then(console.log);
+  .then(console.log)
